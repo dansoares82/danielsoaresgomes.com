e80b507cab0894d3d549c1092fffede4547b13aa
Daniel Soares <39413741+dansoares82@users.noreply.github.com>
2019-11-13 00:04:15 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Daniel Soares Gomes</title>
+    <meta charset="utf-8"/>
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
+    <meta content="Página padrão" name="description"/>
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
+    <meta name="description" content="Site pessoal, de Daniel Soares Gomes"/>
+    <!--Bootstrap-->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+    <!--JS-->
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        a{ font-size:16px; }
+        h3 { font-size:20px; }
+    </style>
+</head>
+
+<body>
+<header>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button class="navbar-toggle" data-target="#myNavbar" data-toggle="collapse" type="button">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Daniel S. Gomes</a>
+            </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="index.php"><i aria-hidden="true" class="fa fa-graduation-cap"></i>Home</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+</header><!--FIM Menu da página-->
+<div class="container-fluid">
+    <h2 class="text-center">Site pessoal<br><small>Neste site estão todos os meus estudos, <a href="#curriculo">currículo profissional,</a> hobbies, pesquisas e outras curiosidades de diversas áreas.<br>Seja bem vindo!</small></h2>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <h3>Curso Superior</h3>
+                    <a href="conteudos/1-faculdade/indexFaculdade.html">Curso de Análise e Desenvolvimento de Sistemas no UniProjeção</a><br>
+                <h3>Curso Técnico</h3>
+                    <a href="conteudos/1.1-Tenfermagem/indexTenfermagem.html">Curso de Técnico em Enfermagem</a>
+                <h3>Informática TI</h3>
+                    <a href="conteudos/5-informatica/dicionarioInformatica.html">Dicionário da Informática</a><br>
+                    <a href="conteudos/5-informatica/SegurancaTI.html">Segurança e Invasões na TI</a><br>
+                    <a href="conteudos/5-informatica/livrosHackerInside.html">Livros Hacker Inside</a><br>
+                    <a href="conteudos/5-informatica/windows.html">Windows</a><br>
+                    <a href="conteudos/5-informatica/livros/indexLivros.html">Livros TI (Resumos)</a><br>
+                <h3>Programação Web, Mobile, Desktop</h3>
+                    <li>Programação Web</li>
+                    <a href="conteudos/5.1-programacao/html.html">HTML</a><br>
+                    <a href="conteudos/5.1-programacao/css.html">CSS</a><br>
+                    <a href="conteudos/5.1-programacao/js.html">JavaScript</a><br>
+                    <a href="conteudos/5.1-programacao/php.html">PHP</a><br>
+                    <li>Programação Multiplataforma</li>
+                    <a href="conteudos/5.1-programacao/linguagemC.html">Linguagem de Programação C</a><br>
+                    <a href="conteudos/5.1-programacao/javaDesktop.html">Java Desktop</a><br>
+                    <li>Outros</li>
+                    <a href="conteudos/5.1-programacao/algoritmos.html">Algoritmos</a>
+                <h3>Hardware e Estudos relacionados no Desenvolvimento de Hardware</h3>
+                    <a href="conteudos/5.2-hardware/devHardware.html">Sobre o Desenvolvimento de Hardware e Áreas de interesse</a>
+                <h3>Redes de Computadores</h3>
+                    <a href="conteudos/5.3-redes/indexRedes.html">Equipamentos de Redes</a>
+                <h3>Perícia Forense na TI & Técnicas Investigativas</h3>
+                    <a href="conteudos/6-periciaInvestigacao/estudosPericiaForenseNaTI.html">Estudos Perícia Forense na TI</a><br>
+                    <a href="conteudos/6-periciaInvestigacao/tecnicasInvestigativasGerais.html">Técnicas Gerais de Investigação</a><br>
+                    <a href="conteudos/6-periciaInvestigacao/crimesJaOcorridos.html">Crimes já Ocorridos</a><br>
+                    <a href="conteudos/6-periciaInvestigacao/estudosPericiaBiologica.html">Estudos Perícias na Biologia</a><br>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <h3>Concursos</h3>
+                    <a href="conteudos/2-concursos/cursoEmAudio/indexCursoEmAudio.html">Curso Em Áudio Matérias</a><br>
+                    <a href="conteudos/2-concursos/provas/indexProvas.html">Provas e Gabaritos</a><br>
+                    <a href="conteudos/2-concursos/teoriaTI/indexTeoriaTI.html">Teoria TI</a><br>
+                    <a href="conteudos/2-concursos/peritoCriminalFederalTI/indexPcfTI.html">Perito C. Federal TI</a><br>
+                    <a href="conteudos/2-concursos/pcdf/indexPcdf.html">Escrivão PC-DF</a><br>
+                    <a href="conteudos/2-concursos/auditorFRF/indexAfrf.html">Auditor Fiscal da RF</a><br>
+                    <a href="conteudos/2-concursos/oficialAbin/indexAbin.html">Oficial Inteligêcia ABIN</a><br>
+                    <a href="conteudos/2-concursos/Tenfermagem/indexEnfermagem.html">Téc. Enfermagem</a><br>
+                <h3>Direito</h3>
+                    <a href="conteudos/9-direito/direitoDoConsumidor.html">Direito do Consumidor</a><br>
+                    <a href="conteudos/9-direito/codigoPenal.html">Código Penal</a><br>
+                <h3>Técnico em Enfermagem</h3>
+                    <a href="conteudos/8-enfermagem/microbiologia.html">Microbiologia</a><br>
+                    <a href="conteudos/8-enfermagem/zoonoses.html">Zoonoses</a><br>
+                    <a href="conteudos/8-enfermagem/procedimentosTratamentosHospitalares.html">Procedimentos e Tratamentos Hospitalares</a><br>
+                    <a href="conteudos/8-enfermagem/"></a><br>
+
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <h3>Cursos Online e Presenciais</h3>
+                <a href="conteudos/3-cursos/FGV">FGV - </a><br>
+                <a href="conteudos/3-cursos/PhpB7Web/cursoPhpB7Web.html">PHP: B7Web Prof. Bonieky</a><br>
+                <a href="conteudos/3-cursos/bancoDeDados.html">Banco de Dados</a><br>
+                <a href="conteudos/3-cursos/cientistaDeDados.html">Cientista de Dados</a><br>
+            <h3>História</h3>
+                    <a href="conteudos/4-historia/brasil/indexBrasil.html">História do Brasil</a><br>
+                    <a href="conteudos/4-historia/historiaCronologica.html">História Cronológica</a>
+            <h3>Ciência</h3>
+                <a href="conteudos/7-ciencia/indexCiencia.html">Ciências</a><br>
+                <a href="conteudos/7-biologia/biologiaHumana.html">Biologia Humana</a><br>
+                <a href="conteudos/7-biologia/biologiaVegetal.html">Biologia Vegetal</a><br>
+                <a href="conteudos/7-biologia/biologiaAnimal.html">Biologia Animal</a><br>
+                <a href="conteudos/7-biologia/venenosEtoxinas.html">Venenos e Toxinas</a><br>
+            <h3>Astronomia</h3>
+                <a href="conteudos/7-ciencia/astronomia/indexAstronomia.html">Astronomia</a><br>
+            <h3>Musculação e Nutrição Alimentar</h3>
+                <a href="conteudos/7.1-nutricaoMusculacao/treinoSemanalDaniel.html">Treino Semanal Daniel</a><br>
+            <h3>Psicologia</h3>
+                <a href="conteudos/8.1-psicologia/livroOcodigoDaInteligencia.html">Livro O Código da Inteligência</a>
+            <h3>Etimologia</h3>
+                <a href="conteudos/etimologia.html">Etimologia - Estudo da Origem e Evolução das Palavras</a><br>
+            <h3>Séries</h3>
+                    <a href="conteudos/0-series/documentarios.html">Documentários</a><br>
+                    <a href="conteudos/0-series/smallville.html">Smallville</a><br>
+                    <a href="conteudos/0-series/csiLV.html">CSI Las Vegas</a><br>
+                    <a href="conteudos/0-series/csiMiami.html">CSI Miami</a><br>
+                    <a href="conteudos/0-series/csiNY.html">CSI New York</a><br>
+                    <a href="conteudos/0-series/ncisLA.html">NCIS Los Angeles</a><br>
+                    <a href="conteudos/0-series/ncisW.html">NCIS Washington</a><br>
+                    <a href="conteudos/0-series/ncisNO.html">NCIS New Orleans</a><br>
+                    <a href="conteudos/0-series/hawaii5-0.html">Hawaii 5-0</a><br>
+        </div>
+    </div><!--div row-->
+    <div style="margin-top:50px;"></div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center"><h2 class="text-center" style="margin-top:20%;" id="curriculo">Currículum Vitae<br>Daniel Soares Gomes</h2></div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 tecr-center">
+            <img src="assets/imagens/danielRosto.jpeg" width="75%" class="img-circle img-responsive" style="margin-left:10%;">
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"></div>
+    </div><!--row curriculo-->
+    <footer class="text-center" style="margin-top:40px; margin-bottom:10px;">Rodapé</footer>
+</div><!--div container-fluid-->
+</body>
+</html>
